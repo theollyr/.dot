12f43371a850c2d2bcea0dddcc74e3ec9e913d5b
Patrik Gajdošík <gajdosik.patrikk@gmail.com>
2022-08-30 23:05:01 +0200

    codestyle: general format fixup

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -433,7 +433,8 @@ return require('packer').startup(function()
             })
 
             vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-                vim.lsp.diagnostic.on_publish_diagnostics, {
+                vim.lsp.diagnostic.on_publish_diagnostics,
+                {
                     underline = false,
                     virtual_text = true,
                     signs = true,
